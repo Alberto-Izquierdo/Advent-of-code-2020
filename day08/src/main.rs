5a0209b979d836077bd16aaa1fa59b3778c0db25
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-12-08 10:47:18 +0100

    Day 8

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -2,12 +2,14 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Lines};
 
+#[derive(Clone)]
 enum Operation {
     NOP,
     ACC,
     JMP,
 }
 
+#[derive(Clone)]
 struct Instruction {
     operation: Operation,
     argument: i32,
@@ -42,27 +44,59 @@ fn execute_instruction(instruction: &Instruction, index: i32, accumulator: i32) 
     }
 }
 
-fn get_accumulator_when_loop_starts(program: Vec<Instruction>) -> i32 {
+fn get_accumulator_when_program_finishes(program: Vec<Instruction>) -> Result<i32, String> {
     let mut accumulator = 0;
     let mut lines_visited = HashSet::new();
     let mut index: i32 = 0;
-    loop {
+    while index < program.len() as i32 {
+        if index < 0 {
+            return Err(String::from("Wrong program found"));
+        }
         let instruction = program.get(index as usize).unwrap();
         let result = execute_instruction(instruction, index, accumulator);
         index = result.0;
         accumulator = result.1;
         match lines_visited.get(&index) {
-            Some(_) => return accumulator,
+            Some(_) => {
+                return Err(String::from("Loop detected"));
+            }
             None => {
                 lines_visited.insert(index);
             }
         }
     }
+    return Ok(accumulator);
+}
+
+fn get_accumulator_of_correct_program(program: Vec<Instruction>) -> i32 {
+    for i in 0..program.len() - 1 {
+        let instruction = program.get(i).unwrap();
+        let new_instruction = match instruction.operation {
+            Operation::JMP => Instruction {
+                operation: Operation::NOP,
+                argument: instruction.argument,
+            },
+            Operation::NOP => Instruction {
+                operation: Operation::JMP,
+                argument: instruction.argument,
+            },
+            _ => continue,
+        };
+        let slices = program.split_at(i + 1);
+        let mut new_program = slices.0.split_last().unwrap().1.to_vec();
+        new_program.push(new_instruction);
+        let new_program = new_program.into_iter().chain(slices.1.to_vec()).collect();
+        match get_accumulator_when_program_finishes(new_program) {
+            Ok(accumulator) => return accumulator,
+            Err(_) => {}
+        }
+    }
+    0
 }
 
 fn main() {
     let lines = BufReader::new(File::open("input.txt").unwrap()).lines();
     let program = read_program(lines);
-    let accumulator = get_accumulator_when_loop_starts(program);
+    let accumulator = get_accumulator_of_correct_program(program);
     println!("Result = {}", accumulator);
 }
