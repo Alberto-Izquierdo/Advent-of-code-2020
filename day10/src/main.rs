00b5321c3f15c05296405f5518cbe679d2499c99
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-12-12 09:59:23 +0100

    Day 10

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -8,6 +8,40 @@ struct JoltDifferences {
     three_jolt: u32,
 }
 
+fn get_list_of_consecutive_numbers_group_sizes(input: Vec<usize>) -> Vec<u32> {
+    let mut current_t = 0;
+    let mut result = vec![];
+    for i in 1..input.len() {
+        if *input.get(i).unwrap() - *input.get(i - 1).unwrap() == 1 {
+            current_t += 1;
+        } else {
+            if current_t != 1 {
+                result.push(current_t);
+                current_t = 1;
+            }
+        }
+    }
+    if current_t != 1 {
+        result.push(current_t);
+    }
+    result
+}
+
+fn get_number_of_possibilities(input: Vec<usize>) -> u64 {
+    let consecutive_group_sizes = get_list_of_consecutive_numbers_group_sizes(input);
+    let possibilities_by_group = consecutive_group_sizes
+        .iter()
+        .filter(|value| **value > 2)
+        .map(|v| match *v {
+            3 => 2,
+            4 => 4,
+            5 => 7,
+            _ => 0,
+        })
+        .collect::<Vec<u64>>();
+    possibilities_by_group.into_iter().fold(1, |v, acc| v * acc)
+}
+
 fn get_jolt_differences(input: BTreeSet<usize>) -> JoltDifferences {
     let mut previous_jolt = 0;
     input.into_iter().fold(
@@ -34,16 +68,14 @@ fn get_jolt_differences(input: BTreeSet<usize>) -> JoltDifferences {
 }
 
 fn main() {
-    let input: BTreeSet<usize> = BufReader::new(File::open("input.txt").unwrap())
-        .lines()
-        .map(|value| value.unwrap().parse::<usize>().unwrap())
+    let input: BTreeSet<usize> = std::iter::once(0)
+        .chain(
+            BufReader::new(File::open("input.txt").unwrap())
+                .lines()
+                .map(|value| value.unwrap().parse::<usize>().unwrap()),
+        )
         .collect();
-    let last = *input.iter().last().unwrap();
-    let input = input.into_iter().chain(std::iter::once(last + 3)).collect();
-    let jolt_differences = get_jolt_differences(input);
-    println!("{:?}", jolt_differences);
-    println!(
-        "Result: {}",
-        jolt_differences.one_jolt * jolt_differences.three_jolt
-    );
+    let vector: Vec<usize> = input.into_iter().collect();
+    let result = get_number_of_possibilities(vector);
+    println!("{}", result);
 }
