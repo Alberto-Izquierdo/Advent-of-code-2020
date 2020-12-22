9f7031b6c59a9445addc38a376e1c18d1d21f310
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-12-22 17:17:17 +0100

    Day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -13,7 +13,7 @@ fn get_initial_values(values: Vec<u32>) -> (u32, HashMap<u32, u32>) {
     )
 }
 
-fn get_last_number(
+fn part_1(
     last_number: u32,
     current_turn: u32,
     max_turn: u32,
@@ -33,14 +33,37 @@ fn get_last_number(
                 0
             }
         };
-        get_last_number(current_number, current_turn + 1, max_turn, previous_numbers)
+        part_1(current_number, current_turn + 1, max_turn, previous_numbers)
     }
+}
+
+fn part_2(
+    last_number: u32,
+    first_turn: u32,
+    max_turn: u32,
+    mut previous_numbers: HashMap<u32, u32>,
+) -> u32 {
+    let mut last_number = last_number;
+    for current_turn in first_turn..max_turn {
+        last_number = match previous_numbers.get(&last_number) {
+            Some(value) => {
+                let result = current_turn - 1 - value;
+                previous_numbers.insert(last_number, current_turn - 1);
+                result
+            }
+            None => {
+                previous_numbers.insert(last_number, current_turn - 1);
+                0
+            }
+        };
+    }
+    return last_number;
 }
 
 fn main() {
     let input = vec![0, 5, 4, 1, 10, 14, 7];
     let initial_turn = input.len() as u32;
     let (last_number, map) = get_initial_values(input);
-    let result = get_last_number(last_number, initial_turn, 2020, map);
+    let result = part_2(last_number, initial_turn, 30000000, map);
     println!("Result: {}", result);
 }
